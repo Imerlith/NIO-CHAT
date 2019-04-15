efd13490bcf9763f25375720b8526beaf4bb401c
Imerlith <36335136+Imerlith@users.noreply.github.com>
2019-04-15 16:45:18 +0200

    Server

--- src/zad1/CommUtils.java
+++ src/zad1/CommUtils.java
@@ -1,11 +1,52 @@
 package zad1;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
 public class CommUtils {
-    public static void writeFromString(){
+    private final static int MAX_LENGTH = 512;
+    private final static Charset charset = Charset.forName("UTF-8");
+    public static void writeFromString(String output, SocketChannel channel){
+        output += "\n";
+        ByteBuffer byteBuffer  = charset.encode(output);
+        while (byteBuffer.hasRemaining()){
+            try {
+                channel.write(byteBuffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
-    public static String readToString(){
-        return "";
+    public static String readToString(SocketChannel channel){
+        if(!channel.isOpen())
+            return "";
+        StringBuilder builder = new StringBuilder();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(MAX_LENGTH);
+        byteBuffer.clear();
+        try {
+            outerloop:
+            while (byteBuffer.hasRemaining()){
+                int n = channel.read(byteBuffer);
+                if (n>0){
+                    byteBuffer.flip();
+                    CharBuffer charBuffer =charset.decode(byteBuffer);
+                    while (charBuffer.hasRemaining()){
+                        char c = charBuffer.get();
+                        if (c=='\n'){
+                            break outerloop;
+                        }
+                        builder.append(c);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
     }
 
 }
--- src/zad1/Server.java
+++ src/zad1/Server.java
@@ -17,65 +17,71 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class Server {
-  private boolean listening ;
-  private ServerSocketChannel serverChannel;
-  private Selector selector;
+    private boolean listening ;
+    private ServerSocketChannel serverChannel;
+    private Selector selector;
 
-  public Server(){
-    try {
-       serverChannel  = ServerSocketChannel.open();
-      serverChannel.socket().bind(new InetSocketAddress(9999));
-      serverChannel.configureBlocking(false);
-       selector  = Selector.open();
-      serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-      listening = true;
-      listenForConnections();
-    } catch (IOException e) {
-      e.printStackTrace();
+    public Server(){
+        try {
+            serverChannel  = ServerSocketChannel.open();
+            serverChannel.socket().bind(new InetSocketAddress(9999));
+            serverChannel.configureBlocking(false);
+            selector  = Selector.open();
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            listening = true;
+            listenForConnections();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
-  private void listenForConnections() throws IOException {
-    while (listening){
-      selector.select();
-      Set keys = selector.selectedKeys();
-      Iterator  iterator = keys.iterator();
-      while(iterator.hasNext()){
-        SelectionKey key =(SelectionKey) iterator.next();
-        iterator.remove();
-        if (key.isAcceptable()){
-          SocketChannel socketChannel  = serverChannel.accept();
-          socketChannel.configureBlocking(false);
-          serverChannel.register(selector,SelectionKey.OP_READ|SelectionKey.OP_WRITE);
-          continue;
+    private void listenForConnections() throws IOException {
+        while (listening){
+            selector.select();
+            Set keys = selector.selectedKeys();
+            Iterator  iterator = keys.iterator();
+            while(iterator.hasNext()){
+                SelectionKey key =(SelectionKey) iterator.next();
+                iterator.remove();
+                if (key.isAcceptable()){
+                    SocketChannel socketChannel  = serverChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    serverChannel.register(selector,SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+                    continue;
+                }
+                if (key.isReadable()){
+                    SocketChannel socketChannel = (SocketChannel)key.channel();
+                    String input = CommUtils.readToString(socketChannel);
+                    if(input.length() > 0){
+                        Message message = new Message(input);
+                        broadcastMessage(message.getFormattedMessage());
+                    }
+                }
+
+            }
+
         }
-        if (key.isReadable()){
-          SocketChannel socketChannel = (SocketChannel)key.channel();
+    }
 
 
-          continue;
-        }
-        if (key.isWritable()){
-          SocketChannel socketChannel = (SocketChannel)key.channel();
 
+    public static void main(String[] args) {
 
-        }
-      }
+        new Server();
 
     }
-  }
-  private void serviceRequest(SocketChannel socketChannel){
-    if (!socketChannel.isOpen()) return;
-
-
-  }
-
-
-  public static void main(String[] args) {
-
-
-
-  }
-  private void broadcastMessega(String message){
-
-  }
+    private void broadcastMessage(String message){
+        try {
+            selector.select();
+            Set keys = selector.selectedKeys();
+            for (Object o : keys) {
+                SelectionKey key = (SelectionKey) o;
+                if(key.isWritable()){
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    CommUtils.writeFromString(message,socketChannel);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
