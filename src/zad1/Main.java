ad517c02a962571a22e358c187d4de12b6226ef5
Imerlith <36335136+Imerlith@users.noreply.github.com>
2019-04-15 16:11:58 +0200

    Main

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -9,7 +9,14 @@ package zad1;
 
 public class Main {
 
-  public static void main(String[] args) {
-
-  }
+    public static void main(String[] args) {
+        new Thread(Server::new).start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new Thread(Client::new).start();
+        new Thread(Client::new).start();
+    }
 }
